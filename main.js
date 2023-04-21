944b2893f0feb142c9348ef09453f608e84e245a
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-04-20 21:49:53 -0500

    orientada

---
+++ main.js
@@ -0,0 +1,41 @@
+class persona{
+    constructor( id,nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+} 
+
+// le doy un class con el objeto que valla a describir
+class auto {
+    constructor(marca,placa,propietario){
+        this.marca = marca;
+        this.placa = placa;
+        this.propietario = propietario;
+       
+    }
+ //arrow function
+    encender = () =>{
+        console.log( "enciende")
+    }
+     //function
+    apagar (){
+        console.log("apaga")
+    }
+
+    acelerar (velocida){
+        this.velocida = velocida
+        console.log("velocidad ${this.velocidad}")
+    }
+}
+
+//instancias
+const objcamilo = new persona( "1061428564","camilo")
+const objandres = new persona( "1061428576","andres")
+
+const auto1 = new auto( "audi", "YTR897","Camilo");
+console.log( auto1 );
+
+auto1.encender();
+auto1.apagar();
+auto1.acelerar( 10 );
+
